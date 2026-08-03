431153d81dfca329649e377d906bf4a33e82e764
Sergiu Cetulean <sergiu.cetulean@gmail.com>
2026-08-03 21:31:08 +0200

    Update in lesson.py

--- Python-Basics/Day02/lesson.py
+++ Python-Basics/Day02/lesson.py
@@ -1,7 +1,7 @@
-def multiply(a, b):
-    return a * b
+def multiply(a, b, c):
+    return a * b * c
 
-result = multiply(5, 3)
+result = multiply(5, 3, 2)
 print("The result of multiplication is:", result)
 
 def greet(name):
